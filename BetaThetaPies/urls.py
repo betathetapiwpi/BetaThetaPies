a567015b2cb32a2dba056c65142071bfd6e5984f
Tim Winters <twinters@wpi.edu>
2017-12-16 17:37:36 -0500

    Move pies to root directory

--- BetaThetaPies/urls.py
+++ BetaThetaPies/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('pies/', include('pies.urls')),
+    path('', include('pies.urls')),
     path('admin/', admin.site.urls),
 ]
