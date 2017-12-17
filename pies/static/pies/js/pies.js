2d91e229b241106d6ded2e09c1641b9e122fee31
Tim Winters <twinters@wpi.edu>
2017-12-17 16:36:04 -0500

    Add Dragon to header. Readd settings.py

--- pies/static/pies/js/pies.js
+++ pies/static/pies/js/pies.js
@@ -1,3 +1,7 @@
+$(document).ready(function(){
+    $('#cellnumber').mask('999-999-9999');
+})
+
 function pay() {
     window.open('https://venmo.com/BetaThetaPi-WPI?txn=pay&note=hello&amount=10', 'Beta Theta Pie Payment');
 }
