e6c39bad4f6dabe925cf3515e7e27504334e920c
Tim Winters <twinters@wpi.edu>
2017-12-17 23:39:28 -0500

    Add time selector to webpage

--- pies/models.py
+++ pies/models.py
@@ -7,6 +7,15 @@ import pytz
 tz = pytz.timezone('US/Eastern')
 class Date(models.Model):
     delivery_time = models.DateTimeField('Delivery Time')
+    ordered = models.BooleanField('Ordered', default=False)
+
 
     def __str__(self):
         return self.delivery_time.astimezone(tz).strftime("%a %b %d, %I:%M %p")
+
+
+    def __eq__(self, other):
+        return str(self) == str(other)
+
+    def __hash__(self):
+        return hash(str(self))
--- pies/views.py
+++ pies/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from pies.models import Date
 
 def index(request):
+    dates = []
+
+    for date in Date.objects.all():
+        if not date.ordered and date not in dates:
+            dates.append(date)
     template = loader.get_template('pies/index.html')
-    return HttpResponse(template.render({}, request))
+    return HttpResponse(template.render({'dates': dates}, request))
 
 # Create your views here.
