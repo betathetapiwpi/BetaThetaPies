01c4c74175d04e2f32979afe0627c6326758dc48
Tim Winters <twinters@wpi.edu>
2018-01-23 20:48:34 -0500

    Please fix pies

--- pies/models.py
+++ pies/models.py
@@ -8,7 +8,7 @@ tz = pytz.timezone('US/Eastern')
 class Date(models.Model):
     delivery_time = models.DateTimeField('Delivery Time')
     ordered = models.BooleanField('Ordered', default=False)
-    name = models.TextField('Name', default = '')
+    name = models.TextField('Name', default=' ')
 
     def __str__(self):
         return self.delivery_time.astimezone(tz).strftime("%a %b %d, %I:%M %p")
