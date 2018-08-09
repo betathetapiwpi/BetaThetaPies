691ea6a64517351c240bd32baec9d5100bc484b1
Tim Winters <twinters@wpi.edu>
2018-08-08 22:03:57 -0400

    Fully convert to Node.js

--- js/index.js
+++ js/index.js
@@ -1,35 +1,133 @@
-var express = require('express');
-const path = require('path')
+const express = require('express');
+const fs = require('fs');
+const request = require('request');
+const readline = require('readline');
+const {google} = require('googleapis');
+
 var app = express();
 
 const PORT = process.env.PORT || 8000;
 
-const { Pool } = require('pg');
-const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: true
-});
-
 app.use(express.static(__dirname + '/../'));
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, '/../views'));
 
 app.get('/', function (req, res) {
     res.render('/index.html');
 });
 
+const {client_secret, client_id, redirect_uris} = JSON.parse(process.env.gapi_credentials).installed;
+const oAuth2Client = new google.auth.OAuth2(
+    client_id, client_secret, redirect_uris[0]);
+oAuth2Client.setCredentials(JSON.parse(process.env.gapi_token));
 
-app.get('/db', async (req, res) => {
-    try {
-        const client = await pool.connect();
-        const result = await client.query('SELECT * FROM test_table');
-        res.render('pages/db', result);
-        client.release();
-    } catch (err) {
-        console.error(err);
-        res.send("Error " + err);
-    }
+google.options({
+    auth: oAuth2Client
 });
 
+oAuth2Client.on('tokens', (tokens) => {
+    oAuth2Client.setCredentials(tokens);
+});
+oAuth2Client.refreshAccessToken();
 
-app.listen(PORT);
+/**
+ * Prints the names and majors of students in a sample spreadsheet:
+ * @see https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
+ * @param {google.auth.OAuth2} auth The authenticated Google OAuth client.
+ */
+function getSheet(){
+    const sheets = google.sheets({version: 'v4', oAuth2Client});
+    sheets.spreadsheets.values.get({
+        spreadsheetId: '1Lq3agSSEOv_OKVymELTnvH1Rj9we0GkZS8bvNWjRoiA',
+        range: 'C2:C81',
+    }, (err, res) => {
+        if (err) return console.log('The API returned an error: ' + err);
+        const rows = res.data.values;
+        if (rows.length) {
+            // Print columns A and E, which correspond to indices 0 and 4.
+            rows.map((row) => {
+                console.log(`${row}`);
+            });
+        } else {
+            console.log('No data found.');
+        }
+    });
+}
+
+app.post('/payments/api', function(req, res){
+    data = JSON.parse(req.body);
+    note = data.data.note;
+    amount = data.data.amount;
+    settled = data.data.status === 'settled';
+
+    if (!note.startswith('BTPOO')){
+        console.log("Not a BTPies Order");
+        res.status(200);
+        res.send();
+    }
+
+    if (!settled || amount < 10 && data.data.actor.username !== 'Tim-Winters-007') {
+        print("Either not settled or too little amount");
+        res.sendStatus(200);
+    }
+
+    const order = note.substring(5).split(',');
+    day = order[4];
+    time = order[5];
+    day = '2018 01 ' + order[4] + ' ' + order[5] + 'PM';
+    date = datetime.strptime(day, '%Y %m %d %I:%M%p').astimezone(timezone('US/Eastern'));
+
+    add_order(order);
+    res.status(200);
+    res.send();
+});
+
+function add_order(order){
+    const name = order[0];
+    const addr = order[1];
+    const cellnumber = order[2];
+    const toppings = order[3];
+    const date = order[4];
+    const time = order[5];
+    const notes = order[6];
+
+    const sheets = google.sheets({version: 'v4', oAuth2Client});
+    sheets.spreadsheets.values.get({
+        spreadsheetId: '1Lq3agSSEOv_OKVymELTnvH1Rj9we0GkZS8bvNWjRoiA',
+        range: 'A2:I81',
+    }, (err, res) => {
+        if (err){
+            console.log(err);
+            return;
+        }
+        const rows = res.data.values;
+        if (rows.length){
+            for(let i = 0; i < rows.length; i++){
+                console.log(typeof rows[i][0]);
+                if (rows[i][0].includes(date)){
+                    for(let j = 0; j < 26; j+=2){
+                        if (rows[i+j][2].includes(time)){
+                            console.log(rows[i+j]);
+                            if(rows[i+j][3] !== ''){j++;}
+                            if(rows[i+j][3] !== ''){console.log("Overbook", order); return;}
+                            sheets.spreadsheets.values.update({
+                                spreadsheetId: '1Lq3agSSEOv_OKVymELTnvH1Rj9we0GkZS8bvNWjRoiA',
+                                range: 'D'+(i+j+2)+':I'+(i+j+2),
+                                valueInputOption: 'USER_ENTERED',
+                                resource: {"values": [[name, cellnumber, addr, toppings, notes, "Venmo"]]}
+                            });
+                            break;
+                        }
+                    }
+                    break;
+                }
+            }
+        }
+    });
+}
+
+app.get('/test', function(req, res){
+    console.log(req.originalUrl);
+    res.send("Hello");
+    getSheet();
+});
+
+app.listen(8000);
--- js/pies.js
+++ js/pies.js
@@ -8,39 +8,39 @@ $(document).ready(function(){
     });
 
 function pay() {
-    name = $('#name').val().replace(',', ' ');
-    addr = $('#address').val().replace(',', ' ');
-    cell = $('#cellnumber').val();
-    var toppings = "";
-    for(i = 0; i <= 7; i++){
+    const name = $('#name').val().replace(',', ' ');
+    const addr = $('#address').val().replace(',', ' ');
+    const cell = $('#cellnumber').val();
+    let toppings = "";
+    for(let i = 0; i <= 7; i++){
         if ($('#toppingsinput-'+i).prop('checked')){
             toppings += $('#toppingsinput-'+i).attr('value')+'/'
         }
     }
-    toppingsum = toppings.slice(0,-1);
-    day = $('#dateselector').val();
-    time = $('#time'+day).find(":selected").text();
-    comments = $('#comments').val().replace(',', ' ');
-    note = name+','+addr+','+cell+','+toppingsum+','+day+','+time+','+comments;
+    const toppingsum = toppings.slice(0,-1);
+    const day = $('#dateselector').val();
+    const time = $('#time'+day).find(":selected").text();
+    const comments = $('#comments').val().replace(',', ' ');
+    const note = name+','+addr+','+cell+','+toppingsum+','+day+','+time+','+comments;
     window.open('https://venmo.com/BetaThetaPi-WPI?txn=pay&note=BTPOO'+note+'&amount=10', '_blank');
 }
 
-function changeTime(day){
+function changeTime(){
     $('#' + last).css("display", "none");
     last = $("#dateselector").val();
     $('#' + last).css("display", "block");
 
 }
 
-function random(){
-    rand = Math.floor(Math.random() * 252) + 3;
-    console.log(rand);
-    for(i = 2; i <= 7; i++){
-        if( (1 << i) & rand){
-            $('#toppingsinput-' + i).prop('checked', true)
-        }
-        else{
-            $('#toppingsinput-' + i).prop('checked', false)
-        }
-    }
-}
+// function random(){
+//     rand = Math.floor(Math.random() * 252) + 3;
+//     console.log(rand);
+//     for(i = 2; i <= 7; i++){
+//         if( (1 << i) & rand){
+//             $('#toppingsinput-' + i).prop('checked', true)
+//         }
+//         else{
+//             $('#toppingsinput-' + i).prop('checked', false)
+//         }
+//     }
+// }
