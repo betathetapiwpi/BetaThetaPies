0a9fdb251bd7f415d8dba53c45fdf1fb4928164a
Tim Winters <twinters@wpi.edu>
2019-01-28 00:45:14 -0500

    Add password protected orders page. Add cash order page.

--- static/js/index.js
+++ static/js/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const auth = require('http-auth');
+const bcrypt = require('bcrypt');
 const path = require('path');
 const bodyParser = require("body-parser");
 const stripe = require("stripe")("sk_test_FuZlPuaOnR30MYPqIxK3Efs5");
-const { Client } = require('pg');
+const {Client} = require('pg');
 
 const client = new Client({
     connectionString: process.env.DATABASE_URL,
@@ -10,25 +12,53 @@ const client = new Client({
 });
 client.connect();
 var app = express();
-app.use(express.static('static'));
-app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+app.use(express.static('static'));
+
+app.use(function (req, res, next) {
+    if ((req.path.indexOf('html') >= 0)) {
+        console.log(req.path);
+        res.sendFile(path.resolve(__dirname + '/../index.html'));
+    }
+    next();
+});
+app.set('view engine', 'html');
+
+app.use(sendViewMiddleware);
 
 
+function sendViewMiddleware(req, res, next) {
+    res.sendView = function (view) {
+        return res.sendFile(path.resolve(path.join(__dirname, "..", view)));
+    };
+    next();
+}
+
+
+let basic = auth.basic({
+    realm: "Private area",
+    file: __dirname + "/../htpasswd"
+});
 const PORT = process.env.PORT || 8000;
 
 
 app.get('/', function (req, res) {
-    res.render('index.html');
+    res.sendView('index.html');
 });
 
-app.get('/orders', function (req, res) {
-    res.render('orders.html');
+app.get('/orders', auth.connect(basic), function (req, res) {
+    res.sendView('orders.html');
+});
+
+app.get('/cash', function (req, res) {
+    res.sendView('neworder.html');
 });
 
 app.get('/orders/orders', function (req, res) {
     let orders = [];
-    client.query("select date, time, name, address, phone, topping, notes from orders order by id", (err, res_) => {
+    client.query("select date, time, name, address, phone, toppings, notes, house from orders order by id", (err, res_) => {
         if (err) throw err;
         for (let row of res_.rows) {
             orders.push(row);
@@ -40,7 +70,7 @@ app.get('/orders/orders', function (req, res) {
 
 app.get('/times', function (req, res) {
     let times = [];
-    client.query("select date || ' ' || time as datetime from orders where name='' group by date, time order by date, time;", (err, res_) => {
+    client.query("select date || ' ' || time as datetime from orders where name='' and (date + time)::timestamp > now() + '2 hour'::interval  group by date, time order by date, time;", (err, res_) => {
         if (err) throw err;
         for (let row of res_.rows) {
             times.push(new Date(row.datetime));
@@ -50,24 +80,55 @@ app.get('/times', function (req, res) {
 });
 
 app.post('/submit', function (req, res) {
-    let body = req.body;
-    const date = body.date;
-    const time = body.time;
-    const name = body.name;
-    const address = body.address;
-    const phone = body.cellnumber;
-    const toppings = [body.sauce,
-        body.cheese,
-        body.pepperoni,
-        body.bacon,
-        body.onion,
-        body['green pepper'],
-        body.sausage,
-        body.mushroom].join('/');
-    const notes = body.notes;
-    const finalNote = [date, time, name, address, phone, toppings, notes].join();
-    return res.redirect("venmo://paycharge?txn=pay&recipients=BetaThetaPi-WPI&amount=10&note=BTPOO" + finalNote);
+    const hash = '$2b$04$AR5iY3E1WcN/Ni.vVnk62eHSg6EDOPqfesBYJ32cxmMafWPS7yrOG';
+    console.log(req.headers.referer);
+    if (req.headers.referer.includes('cash')) {
+        let body = req.body;
+        const date = body.date;
+        const time = body.time;
+        const name = body.name;
+        const address = body.address;
+        const phone = body.cellnumber;
+        const password = body.password;
 
+        const toppings = [body.sauce,
+            body.cheese,
+            body.pepperoni,
+            body.bacon,
+            body.onion,
+            body['green pepper'],
+            body.sausage,
+            body.mushroom].filter(Boolean).join('/');
+        const notes = body.notes;
+        const finalNote = [date, time, name, address, phone, toppings, notes].join();
+        bcrypt.compare(password, hash, function (err, res_) {
+            if (res_) {
+                console.log('Adding cash order');
+                add_order([name, address, phone, toppings, date, time, notes, 'cash']);
+            } else {
+                res.send('<script>alert("Incorrect Password. Order did not go through.")</script>');
+            }
+
+        });
+    } else {
+        let body = req.body;
+        const date = body.date;
+        const time = body.time;
+        const name = body.name;
+        const address = body.address;
+        const phone = body.cellnumber;
+        const toppings = [body.sauce,
+            body.cheese,
+            body.pepperoni,
+            body.bacon,
+            body.onion,
+            body['green pepper'],
+            body.sausage,
+            body.mushroom].filter(Boolean).join('/');
+        const notes = body.notes;
+        const finalNote = [date, time, name, address, phone, toppings, notes].join();
+        return res.redirect("venmo://paycharge?txn=pay&recipients=BetaThetaPi-WPI&amount=10&note=BTPOO" + finalNote);
+    }
 });
 
 app.post('/checkout', function (req, res) {
@@ -86,7 +147,6 @@ app.post('/api/purchase', function (req, res) {
     let data = req.body;
     let note = data.data.note;
     let amount = data.data.amount;
-    console.log(note);
     let settled = data.data.status === 'settled';
 
     if (!note.startsWith('BTPOO')) {
@@ -98,18 +158,20 @@ app.post('/api/purchase', function (req, res) {
 
     if (!settled || (amount < 10 && data.data.actor.username !== 'Tim-Winters-007')) {
         order.concat(['Venmo: Only paid ' + amount]);
-    }
-    else{
-        order.concat('Venmo');
+    } else {
+        order.concat(['Venmo']);
     }
 
     add_order(order);
     res.status(200);
     res.send();
 });
-
+app.all('*', function (req, res) {
+    res.sendView('index.html');
+});
 
 function add_order(order) {
+    console.log('adding order');
     const name = order[0];
     const addr = order[1];
     const cellnumber = order[2];
@@ -118,10 +180,13 @@ function add_order(order) {
     const time = order[5];
     const notes = order[6];
     const meth = order[7];
-    client.connect();
-    client.query("UPDATE order set name = $1 address=$2 phone=$3 toppings=$4 notes=$5 paidwith=$6 where id = " +
-        "(Select id from orders where date='$7' and time='$8' and name = '' limit 1)",
-        name, addr, cellnumber, toppings, notes, meth, date, time);
+    client.query("UPDATE orders set name = $1, address=$2, phone=$3, toppings=$4, notes=$5, paidwith=$6 where id = " +
+        "(Select id from orders where date= $7 and time= $8 and name = '' limit 1)",
+        [name, addr, cellnumber, toppings, notes, meth, date, time], (err, res) => {
+            if (err) {
+                console.log(err);
+            }
+        });
 }
 
 console.log("Ready");
